13992236d220f0ad4010677d095294d8e90fa5ef
allisbri <allisbri@oregonstate.edu>
2018-10-21 05:51:37 -0400

    working on validation

--- wk3/server.js
+++ wk3/server.js
@@ -64,7 +64,7 @@ function get_slip(id){
 function get_ship(id){
     //returns undefined if id does not exist
         const key = datastore.key([SHIP, parseInt(id,10)]);
-        console.log("logging key" + key);
+        //console.log("logging key" + key);
         return datastore.get(key).then(results => {
             //returns entity if id does exist
             const entity = results[0];
@@ -87,8 +87,67 @@ function put_ship(id, name, type, length){
 
 function delete_ship(id){
     const key = datastore.key([SHIP, parseInt(id,10)]);
+    const ship = datastore.get(key);
+    const slips = get_slips();
+    const blank1 = '';
+    const blank2 = '';
+    //console.log("test!");
+    const slipQuery = datastore.createQuery(SLIP);
+    datastore.runQuery(slipQuery).then( (entities) => {
+        //gcloud documentation
+        const slips = entities[0].map(fromDatastore);
+        //console.log(slips.length);
+        for (var i = 0; i < slips.length; i++){
+        //console.log("in loop");
+        if (slips[i].current_boat == id){
+            console.log("found boat");
+            put_slip(slips[i].id, slips[i].number, blank1, blank2);
+        }
+    }
+    });
+    
     return datastore.delete(key);
 }
+
+function delete_all_slips(){
+    const slipQuery = datastore.createQuery(SLIP);
+    //got error unless used return here. Has something to do with promises, but not sure
+    //exactly what the problem is
+    return datastore.runQuery(slipQuery).then( (entities) => {
+        //gcloud documentation
+        const slips = entities[0].map(fromDatastore);
+        //console.log(slips.length);
+        for (var i = 0; i < slips.length; i++){
+        //console.log("in loop");
+            //console.log("found boat");
+            delete_slip(slips[i].id);
+        }
+        //Did not complete request unless return statement was here. Seems that
+        //promises require this, but needs more investigation. *This was actually
+        //an issue with the response.
+        //return true;
+    });
+}
+
+function delete_all_ships(){
+    const shipQuery = datastore.createQuery(SHIP);
+    //got error unless used return here. Has something to do with promises, but not sure
+    //exactly what the problem is
+    return datastore.runQuery(shipQuery).then( (entities) => {
+        //gcloud documentation
+        const ships = entities[0].map(fromDatastore);
+        //console.log(slips.length);
+        for (var i = 0; i < ships.length; i++){
+        //console.log("in loop")
+            delete_ship(ships[i].id);
+        }
+        //Did not complete request unless return statement was here. Seems that
+        //promises require this, but needs more investigation. *This was actually
+        //an issue with the response.
+        //return true;
+    });
+}
+
 
 function delete_slip(id){
     const key = datastore.key([SLIP, parseInt(id,10)]);
@@ -97,30 +156,28 @@ function delete_slip(id){
 
 function dock(slip_id, ship_id, arrival_date){
     const key = datastore.key([SLIP, parseInt(slip_id,10)]);
-    datastore.get(key).then(result => {
+    return datastore.get(key).then( result => {
         const slip = result[0];
         if (!(slip.current_boat)){
             slip.current_boat = ship_id;
             slip.arrival_date = arrival_date;
-            return datastore.save({"key":key, "data":slip});
+            datastore.save({"key":key, "data":slip});
+            return true;
         }
         else {
-            return undefined;
+            return false;
         }
     });
 }
 
 function undock(slip_id, ship_id){
      const key = datastore.key([SLIP, parseInt(slip_id,10)]);
-    datastore.get(key).then(result => {
+    return datastore.get(key).then(result => {
         const slip = result[0];
-        if (!(slip.current_boat == ship_id)){
+        if (slip.current_boat == ship_id){
             slip.current_boat = '';
             slip.arrival_date = '';
             return datastore.save({"key":key, "data":slip});
-        }
-        else {
-            return undefined;
         }
     });
 }
@@ -137,36 +194,49 @@ router.get('/ships', function(req, res){
 });
 
 router.post('/ships', function(req, res){
-    console.log(req.body);
-    post_ship(req.body.name, req.body.type, req.body.length)
-    .then( key => {res.status(200).send('{ "id": ' + key.id + ' }')} );
+    //console.log(req.body);
+    //console.log(typeof req.body.name);
+    //console.log(typeof req.body.length);
+    if ((typeof req.body.name == 'string') && (typeof req.body.type == 'string')
+     && (typeof req.body.length == 'number')) {
+        post_ship(req.body.name, req.body.type, req.body.length)
+        .then( key => {res.status(200).send('{ "id": ' + key.id + ' }')});
+    }
+    else{
+        res.status(403).send();
+    }
 });
 
 
 router.get('/slips', function(req, res){
     const slips = get_slips()
     .then( (slips) => {
-        console.log(slips);
+        //console.log(slips);
         res.status(200).json(slips);
     });
 });
 
 router.post('/slips', function(req, res){
-    console.log(req.body);
-    post_slip(req.body.number)
-    .then( key => {res.status(200).send('{ "id": ' + key.id + ' }')} );
+    //console.log(req.body);
+    if (typeof req.body.number == 'number') {
+         post_slip(req.body.number)
+        .then( key => {res.status(200).send('{ "id": ' + key.id + ' }')} );
+    }
+    else{
+        res.status(403).send();
+    }
 });
 
 router.get('/slips/:id', function(req, res){
         get_slip(req.params.id).then( (slip) => {
-        console.log(slip);
+        //console.log(slip);
         res.status(200).json(slip);
     });
 });
 
 router.get('/ships/:id', function(req, res){
     get_ship(req.params.id).then( (ship) => {
-        console.log(ship);
+        //console.log(ship);
         res.status(200).json(ship);
     });
 });
@@ -182,22 +252,36 @@ router.put('/ships/:id', function(req, res){
     .then(res.status(200).end());
 });
 
-router.delete('slips/:id', function(req, res){
+router.delete('/slips/:id', function(req, res){
     delete_slip(req.params.id).then(res.status(200).end());
 });
 
-router.delete('ships/:id', function(req, res){
+router.delete('/ships/:id', function(req, res){
     delete_ship(req.params.id).then(res.status(200).end());
 });
 
-router.delete('slips/:slip_id/ships/:ship_id', function(req, res){
+router.delete('/ships', function(req, res){
+    delete_all_ships().then(res.status(200).end());
+});
+
+router.delete('/slips', function(req, res){
+    delete_all_slips().then(res.status(200).end());
+});
+
+router.delete('/slips/:slip_id/ships/:ship_id', function(req, res){
     undock(req.params.slip_id, req.params.ship_id).then(res.status(200).end());
 });
 
-router.post('slips/:slip_id/ships/:ship_id', function(req, res){
-   dock(req.params.slip_id, req.params.ship_id, arrival_date).then( 
-    key => {res.status(200).send('{ "id": ' + key.id + ' }')}, () =>
-        res.status(403).send('That slip is occupied')) ;
+router.post('/slips/:slip_id/ships/:ship_id', function(req, res){
+   dock(req.params.slip_id, req.params.ship_id, req.body.arrival_date).then( (value) => {
+    if (value == true){
+        res.status(200).end();
+    }
+    else{
+        res.status(403).end();
+    }
+    })
+  
 });
 
 
